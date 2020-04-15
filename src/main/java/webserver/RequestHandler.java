ba7cc8567db74d67151ac86fc4fd8e0532e1d8ec
suhyunS123 <suhyun709@naver.com>
2020-04-15 23:58:36 +0900

    html 띄우기

--- src/main/java/webserver/RequestHandler.java
+++ src/main/java/webserver/RequestHandler.java
@@ -1,10 +1,14 @@
 package webserver;
 
+import java.io.BufferedReader;
 import java.io.DataOutputStream;
+import java.io.File;
 import java.io.IOException;
 import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.io.OutputStream;
 import java.net.Socket;
+import java.nio.file.Files;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -21,13 +25,27 @@ public class RequestHandler extends Thread {
     public void run() {
         log.debug("New Client Connect! Connected IP : {}, Port : {}", connection.getInetAddress(),
                 connection.getPort());
-
         try (InputStream in = connection.getInputStream(); OutputStream out = connection.getOutputStream()) {
+        	BufferedReader br=
+        			new BufferedReader(new InputStreamReader(in, "UTF-8"));
+        	String line=br.readLine();
+        	if(line==null) {
+        		return;
+        	}
+        	String[] splited=line.split(" ");
+        	String path = splited[1];
+        	log.debug("request path : {}", path);
+        	/*while(!"".equals(line)) {
+        		log.debug("request line:{}",line);	
+        		line=br.readLine(); 
+        	}*/
             // TODO 사용자 요청에 대한 처리는 이 곳에 구현하면 된다.
             DataOutputStream dos = new DataOutputStream(out);
-            byte[] body = "Hello World1".getBytes();
+            byte[] body = Files.readAllBytes(new File("./webapp"+path).toPath());
             response200Header(dos, body.length);
             responseBody(dos, body);
+            
+        
         } catch (IOException e) {
             log.error(e.getMessage());
         }
